305ab2b47de7a0437a56b592d2febe29a9c1bd37
Jessica Duell <jessica.duell@pendo.io>
2021-07-14 12:53:27 -0400

    started testing for put endpoint

--- animals/animalHelpers.js
+++ animals/animalHelpers.js
@@ -1,5 +1,6 @@
 module.exports = {
-    validateAnimal
+    validateAnimal,
+    validateAnimalEdit
 }
 
 function validateAnimal(req, res, next) {
@@ -18,6 +19,39 @@ function validateAnimal(req, res, next) {
             typeof animal.description !== 'string'
             || typeof animal.news_item !== 'string'
             || typeof animal.pic !== 'string'
+        ) {
+            return res.status(400).json({
+                error:
+                    "The request object attributes have one or more of the wrong type",
+                stack: "Animal helpers line 25",
+            });
+        } else {
+            next();
+        }
+    } else {
+        return res.status(400).json({
+            error: 'The request object is missing one or more required attributes',
+            stack: "Animal helpers line 33"
+        })
+    }
+}
+
+function validateAnimalEdit(req, res, next) {
+    let animal = req.body;
+
+    if (
+        animal.description ||
+        animal.news_item ||
+        animal.pic || animal.date
+    ) {
+        if (animal.date && !(animal.date instanceof Date)) {
+            animal.date_created = new Date();
+        }
+
+        if (
+            (animal.description && typeof animal.description !== 'string')
+            || (animal.news_item && typeof animal.news_item !== 'string')
+            || (animal.pic && typeof animal.pic !== 'string')
         ) {
             return res.status(400).json({
                 error:
--- api/server.test.js
+++ api/server.test.js
@@ -378,5 +378,46 @@ describe("server", () => {
         expect(animals).toHaveLength(4)
       })
     })
+
+    describe.only('PUT /animals/:animal_id', ()=>{
+      it('edits the information of an animals based on the animal_id passed in the url', async ()=>{
+        const res = await supertest(server).post("/auth/register").send({
+          username: "sam",
+          password: "pass",
+          first_name: "Sam",
+          last_name: "Gamgee",
+          email: "baggins@gmail.com",
+          admin: false,
+        });
+        const token = res.body.token;
+
+        const testAnimals = await getTestAnimals();
+        await asyncForEach(testAnimals, async (animal) => {
+          await db('animals').insert(animal)
+        })
+
+        const expectedAnimals = await getExpectedTestAnimals();
+        let expectedAnimal = expectedAnimals[0]
+
+        const newDes = 'A wonderful dog to have in the family.'
+        expectedAnimal.description = newDes;
+
+        const res2 = await supertest(server).put("/animals/1").set('authorization', token).send({
+          description: newDes
+        })
+
+        expect(res2.body.message).toBe("Edited 1 animal(s) successfully")
+
+        const dbAnmials = await db('animals');
+        for(let i = 0; i < dbAnmials.length; i++){
+          if(dbAnmials[i].animal_id === expectedAnimal.animal_id){
+            dbAnmials[i].pic = atob(dbAnmials[i].pic)
+            expect(dbAnmials[i]).toEqual(expectedAnimal);
+          }
+        }
+        
+
+      })
+    })
   })
 });
