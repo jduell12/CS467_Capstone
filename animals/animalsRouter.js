d966bbe3a8e43c80d53ebf2a5b183f910f21efb8
Jessica Duell <jessica.duell@pendo.io>
2021-07-14 16:14:57 -0400

    tests passing for getting animal by filter name

--- animals/animalsRouter.js
+++ animals/animalsRouter.js
@@ -15,6 +15,20 @@ router.get('/', (req, res) =>{
     })
 })
 
+router.get('/:filter_name/:filter_value', (req, res) => {
+    let filter = req.params.filter_name;
+    let value = req.params.filter_value;
+    Animals.getAnimalBy(filter, value).then(animalArr => {
+        res.status(200).json({animalArr})
+    }).catch(err => {
+        res.status(500).json({
+            error: err.message,
+            errorMessage: 'Can get get the animal from the database',
+            stack: 'Animal router line 27'
+        })
+    })
+})
+
 router.put('/:animal_id', helpers.validateAnimalEdit, (req, res) =>{
     Animals.getAnimalBy('animal_id', req.params.animal_id).then(animalArr => {
         let animal = animalArr[0]
@@ -25,20 +39,20 @@ router.put('/:animal_id', helpers.validateAnimalEdit, (req, res) =>{
                 res.status(500).json({
                     error: err.message,
                     errorMessage: "Could not edit animal in database",
-                    stack: "Animals Router line 28"
+                    stack: "Animals Router line 42"
                 })
             })
         } else {
             res.status(404).json({
                 message: "No animal with that id",
-                stack: 'Animals Router line 34'
+                stack: 'Animals Router line 48'
             })
         }
     }).catch(err => {
         res.status(500).json({
             error: err.message, 
             errorMessage: "Could not retrieve animal from database",
-            stack: "Animals Router line 41"
+            stack: "Animals Router line 55"
         })
     })
 })
@@ -50,7 +64,7 @@ router.post('/', helpers.validateAnimal, (req,res) =>{
         res.status(500).json({
             error: err.message,
             errorMessage: "Could not add adnimal to database",
-            stack: "Animals Router line 53"
+            stack: "Animals Router line 67"
         })
     })
 })
